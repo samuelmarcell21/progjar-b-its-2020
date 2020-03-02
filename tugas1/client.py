c538715e6beec085a6a1310bf4457853a18b8c50
samuelmarcell21 <samuelmarcell21@gmail.com>
2020-03-02 16:20:15 +0700

    done

--- tugas1/client.py
+++ tugas1/client.py
@@ -5,7 +5,7 @@ import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
-server_address = ('localhost', 31000)
+server_address = ('localhost', 31002)
 print(f"connecting to {server_address}")
 sock.connect(server_address)
 
